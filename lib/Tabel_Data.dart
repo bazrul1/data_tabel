227d7a3146d0032f774f632508bf62f078a341e0
MD. Bazrul Haque Sobuj <md.bazrulhaquesobuj1@gmail.com>
2025-05-20 17:14:11 +0600

    ..

---
+++ lib/Tabel_Data.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabelData extends StatefulWidget {
+  const TabelData({ Key? key }) : super(key: key);
+
+  @override
+  _TabelDataState createState() => _TabelDataState();
+}
+
+class _TabelDataState extends State<TabelData> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
